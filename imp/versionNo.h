e2e2c6b1a0fc42cb20b4e9a7df649f95d9a3de8d
AlexAkatyev <aleksey.akatyev@gmail.com>
2024-03-20 09:29:06 +0400

    Изменить версию

--- imp/versionNo.h
+++ imp/versionNo.h
@@ -4,12 +4,12 @@
 #define DEF_VERSION_MAJOR   21
 #define DEF_VERSION_MINOR   5
 #define DEF_VERSION_PATCH   6
-#define DEF_VERSION_TEST    1
+#define DEF_VERSION_TEST    2
 
 
 #define FILE_VERSION_MAIN DEF_VERSION_MAJOR,DEF_VERSION_MINOR,DEF_VERSION_PATCH,1
 
-#define FILE_VERSION_STR "21. 5. 6. 1\0"
+#define FILE_VERSION_STR "21. 5. 6. 2\0"
 
 #define FILE_COMMENT_STR "Телефон службы поддержки (812) 534-68-82\0"
 
@@ -17,6 +17,6 @@
 
 #define FILE_PRODUCT_STR "Измеритель микроперемещений\0"
 
-#define ANOTHER_PRODUCT_STR "UV 21.5.6.1\0"
+#define ANOTHER_PRODUCT_STR "UV 21.5.6.2\0"
 
 #endif // VERSIONNO_H
